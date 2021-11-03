65f680300be0b0aef4e7b507e63e4cb1545a60a9
R. G. Esteves <rodolfo.g.esteves@intel.com>
2021-11-03 16:10:09 -0700

    Start to look at folds and traversals

--- src/Main.hs
+++ src/Main.hs
@@ -8,15 +8,23 @@ import Lens.Micro.Mtl -- for `view` for now (could be using `Lens.Micro.Extras` 
 
 data Atom = Atom { _element :: String, _point :: Point } deriving (Show)
 data Point = Point { _x :: Double, _y :: Double } deriving (Show)
+data Molecule = Molecule { _atoms :: [Atom] } deriving (Show)
 
 makeLenses ''Atom
 makeLenses ''Point
+makeLenses ''Molecule
+
+atom = Atom { _element = "C", _point = Point { _x = 1.0, _y = 2.0 } }
+atom1 = Atom { _element = "C", _point = Point { _x = 1.0, _y = 2.0 } }
+atom2 = Atom { _element = "O", _point = Point { _x = 3.0, _y = 4.0 } }
+molecule = Molecule { _atoms = [atom1, atom2] }
 
 main :: IO ()
 main = do
-  let atom = Atom { _element = "C", _point = Point { _x = 1.0, _y = 2.0 } }
+  -- let atom = Atom { _element = "C", _point = Point { _x = 1.0, _y = 2.0 } }
   --putStrLn "hello atom " ++ ( atom ^. point . x )
   putStrLn "hello atom " 
   -- putStrLn ( atom ^. point . x )
+  -- toListOf (atoms . folded . point ) molecule
   putStrLn ( atom ^. element )
   putStrLn ( view element atom)
